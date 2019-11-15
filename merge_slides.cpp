fb78804f808300e2664267bea2d87ad40ab750a1
2018009107 <zugzwang99@naver.com>
2019-11-15 23:20:18 +0900

    including exception handling

--- merge_slides.cpp
+++ merge_slides.cpp
@@ -37,6 +37,11 @@ int main() {
         // Parsing input string
         string num = "";
         for (int i = 0; i < str.size(); i++) {
+            // Exception handling
+            if(str[i] != ' ' && (str[i] < 48 || 57 < str[i])){
+                fprintf(stderr, "wrong input included(input must be numbers).\n");
+                exit(1);
+            }
             if (str[i] == ' ') {
                 v.push_back(atoi(num.c_str()));
                 num = "";
@@ -45,28 +50,25 @@ int main() {
         }
         v.push_back(atoi(num.c_str()));
 
-        if (v.size() != 1) {
-            sum = (int*)malloc(sizeof(int) * v.size());
-            for (int i = 0; i < v.size(); i++) {
-                if (i == 0) sum[i] = v[i];
-                else sum[i] = sum[i - 1] + v[i];
-            }
-
-            dp = (int**)malloc(sizeof(int*) * v.size());
-            for (int i = 0; i < v.size(); i++) {
-                dp[i] = (int*)malloc(sizeof(int) * v.size());
-                for (int j = 0; j < v.size(); j++) dp[i][j] = INF;
-            }
-            for (int i = 0; i < v.size(); i++)
-                for (int j = 0; j < v.size(); j++) dp[i][j] = INF;
-
-            printf("%d\n", mergeSlides(0, v.size() - 1));
-
-            for (int i = 0; i < v.size(); i++) free(dp[i]);
-            free(dp);
-            free(sum);
+        sum = (int*)malloc(sizeof(int) * v.size());
+        for (int i = 0; i < v.size(); i++) {
+            if (i == 0) sum[i] = v[i];
+            else sum[i] = sum[i - 1] + v[i];
         }
-        else printf("%d\n", v[0]);
+
+        dp = (int**)malloc(sizeof(int*) * v.size());
+        for (int i = 0; i < v.size(); i++) {
+            dp[i] = (int*)malloc(sizeof(int) * v.size());
+            for (int j = 0; j < v.size(); j++) dp[i][j] = INF;
+        }
+        for (int i = 0; i < v.size(); i++)
+            for (int j = 0; j < v.size(); j++) dp[i][j] = INF;
+
+        printf("%d\n", mergeSlides(0, v.size() - 1));
+
+        for (int i = 0; i < v.size(); i++) free(dp[i]);
+        free(dp);
+        free(sum);
     }
     return 0;
 }
